956e713e6e1a2afed48904eaae91c88e07e7f070
tormich@gmail.com <tormich@gmail.com>
2015-10-24 18:06:55 +0300

    TODO: updateApiUrls

--- routes/api/status.js
+++ routes/api/status.js
@@ -2,14 +2,16 @@ var express = require('express');
 var https = require('https');
 var router = express.Router();
 
-/* GET apiStatus listing. */
-router.get('/', function (req, res, next) {
-    // https://status.github.com/api.json
 
+var lastRefresh = 0
+    , apiUrls = null;
+
+
+var getJson = function(host, path, ondata, onerror){
     var options = {
-        hostname: 'status.github.com'
+        hostname: host
         , port:  443
-        , path: '/api.json'
+        , path: path
         , method: 'GET'
         , headers: {'Content-Type': 'application/json'}
     };
@@ -19,15 +21,54 @@ router.get('/', function (req, res, next) {
         console.log("headers: ", _res.headers);
 
         _res.on('data', function(d) {
-            //process.stdout.write(d);
-            res.end(d);
+            console.log(d);
+            ondata(d);
         });
     });
     _req.end();
 
     _req.on('error', function(e) {
         console.error(e);
+        onerror(e);
     });
+};
+
+var Pipe = function(){
+    return {
+        queue: []
+        , onerrorCallback: null
+
+        , exec: function(onError){
+            this.onerrorCallback = onError;
+        }
+        , next: function(prevResult){
+
+        }
+        , add: function (foo) {
+            // pipelineFunction(prevResult, success, error)
+            this.queue.push(foo);
+        }
+    };
+}
+
+var updateApiUrls = function(){};
+
+/* GET apiStatus listing. */
+router.get('/', function (req, res, next) {
+    // https://status.github.com/api.json
+    var pipe = new Pipe();
+    pipe.add(updateApiUrls);
+
+    if(apiUrls == null){
+        getJson("status.github.com", "/api.json", "GET"
+        , function(data){
+                res.end(data);
+            }
+        , function(error){
+                res.end("error", error);
+            });
+    }
+
 });
 
 module.exports = router;
