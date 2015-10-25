71896839401239c9be83c6586d9a04b448479bc2
tormich@gmail.com <tormich@gmail.com>
2015-10-25 14:28:53 +0200

    working client-server

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,33 @@
 var app = angular.module("MyApp", []);
 
-app.controller("PostsCtrl", function($scope, $http) {
+var buildMsg = function(date, status, body){
+    var options = {
+        weekday: "long", year: "numeric", month: "numeric",
+        day: "numeric", hour: "2-digit", minute: "2-digit"
+    };
+    // 1/1/2013 20:00-22:00: Good: Battlestation fully operational
+    r = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear()
+        + " " + date.getHours()
+        + ":" + date.getMinutes()
+        + ": " + status
+        + ": " + body;
+    return r;
+};
+
+app.controller("MsgCont", function($scope, $http) {
 
     $http.get('/api/status').
         success(function(data, status, headers, config) {
-            $scope.posts = data;
-            console.log(data);
+            msgs = [];
+            for(i in data.messages){
+                msg = data.messages[i];
+                msgs.push(
+                    buildMsg(new Date(msg["created_on"]), msg["status"], msg["body"])
+                )
+            }
+            $scope.msgs = msgs;
+
+            console.log(msgs);
         }).
         error(function(data, status, headers, config) {
             //console.log(apiStatus);
--- routes/api/status.js
+++ routes/api/status.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var https = require('https');
+var pipeline = require('node-pipeline');
+
 var router = express.Router();
 
-
+const GH_HOST = "status.github.com";
 var lastRefresh = 0
-    , apiUrls = null;
+    , apiUrls = null
+    , ghStatus = ""
+    , ghMsgs = [];
 
 
 var getJson = function(host, path, ondata, onerror){
@@ -15,59 +19,85 @@ var getJson = function(host, path, ondata, onerror){
         , method: 'GET'
         , headers: {'Content-Type': 'application/json'}
     };
+    var onData = ondata;
+    var onError = onerror;
 
     var _req = https.request(options, function(_res) {
-        console.log("statusCode: ", _res.statusCode);
-        console.log("headers: ", _res.headers);
-
-        _res.on('data', function(d) {
-            console.log(d);
-            ondata(d);
-        });
+        _res.on('data', (function(d) {
+            onData(JSON.parse(d));
+        }).bind(this));
     });
     _req.end();
 
-    _req.on('error', function(e) {
+    _req.on('error', (function(e) {
         console.error(e);
-        onerror(e);
-    });
+        onError(e);
+    }).bind(this));
 };
 
-var Pipe = function(){
-    return {
-        queue: []
-        , onerrorCallback: null
-
-        , exec: function(onError){
-            this.onerrorCallback = onError;
-        }
-        , next: function(prevResult){
-
-        }
-        , add: function (foo) {
-            // pipelineFunction(prevResult, success, error)
-            this.queue.push(foo);
-        }
-    };
-}
-
-var updateApiUrls = function(){};
 
 /* GET apiStatus listing. */
 router.get('/', function (req, res, next) {
-    // https://status.github.com/api.json
-    var pipe = new Pipe();
-    pipe.add(updateApiUrls);
+    var pl = pipeline.create("Tax and Gratuity Calculator");
+    pl.on('end', function(err, results) {
+        if (err) {
+            console.log('Error in pipeline: ' + err);
+            res.end(JSON.stringify({"error": {"message": 'Error in pipeline: ' + err}}));
+        }
 
-    if(apiUrls == null){
-        getJson("status.github.com", "/api.json", "GET"
-        , function(data){
-                res.end(data);
-            }
-        , function(error){
-                res.end("error", error);
-            });
+        res.end(JSON.stringify({"messages": ghMsgs, "status": ghStatus}));
+    });
+
+    if(apiUrls == null) {
+        pl.use(function (result, next) {
+            getJson(GH_HOST, "/api.json"
+                , function(data){
+                    console.log(data);
+                    apiUrls = data;
+                    next();
+                }
+                , function(error){
+                    res.end(JSON.stringify({"error": {"message": error}}));
+                });
+        });
     }
+
+    if(lastRefresh < (new Date().getTime() - 3600000)) { // one hour
+        pl.use(function (result, next) {
+            getJson(GH_HOST, apiUrls["status_url"].replace("https://", "").replace(GH_HOST, "")
+                , (function(data){
+                    ghStatus = data;
+                    console.log(data);
+                    next();
+                }).bind(this)
+                , (function(){
+                    res.end(JSON.stringify({"error": {"message": error}}));
+                }).bind(this));
+            console.log("1");
+
+        });
+
+        pl.use(function(result, next){
+            getJson(GH_HOST, apiUrls["messages_url"].replace("https://", "").replace(GH_HOST, "")
+                , (function(data){
+                    ghMsgs = data;
+                    console.log(data);
+                    next();
+                }).bind(this)
+                , (function(){
+                    res.end(JSON.stringify({"error": {"message": error}}));
+                }).bind(this));
+            console.log("2");
+
+        });
+    }
+    //pipe.add(updateApiUrls);
+    //pipe.add(getStatu);
+    //pipe.add(getMessages);
+    console.log(lastRefresh);
+    pl.execute();
+
+
 
 });
 
